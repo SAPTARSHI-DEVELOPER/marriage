52bbc7aeb4beb1602c08d5740d2c98e14ebb3e6f
SAPTARSHI-DEVELOPER <saptarshioff@gmail.com>
2018-09-03 11:47:30 +0530

    New Dotnet Core Tried

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace marriage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+        }
+
+        private static void NewMethod()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
